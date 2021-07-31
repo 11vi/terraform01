d0f3856c58f75974047bbddb1e7a902c66cef11d
vikram <vikram.y24@gmail.com>
2021-07-31 12:09:53 +0530

    var

--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -13,3 +13,13 @@ variable "sample2" {}
 output "sample2" {
   value = "var.samlple2"
 }
+output "string" {
+  value = "hello world"
+}
+
+output "number" {
+  value = "10"
+}
+output "booleam" {
+  value = "false"
+}
