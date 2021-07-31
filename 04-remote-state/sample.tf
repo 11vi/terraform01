53b240d37ab1015331d44f8f22b32ac2f3227e65
vikram <vikram.y24@gmail.com>
2021-07-31 16:39:29 +0530

    sample

---
+++ 04-remote-state/sample.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "sample" {
+  ami                   = "ami-074df373d6bafa625"
+  instance_type         = "t3.micro"
+  tags                  = {
+    Name                = "Sample"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
