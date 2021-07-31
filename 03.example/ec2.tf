6848df7787a3b03913d34a39181f7e6416d58e90
vikram <vikram.y24@gmail.com>
2021-07-31 16:30:16 +0530

    example

--- 03.example/ec2.tf
+++ 03.example/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_ssh" {
 
   egress {
     from_port           = 0
-    to_port             = 65535
+    to_port             = 0
     protocol            = "-1"
     cidr_blocks         = ["0.0.0.0/0"]
   }
@@ -36,7 +36,7 @@ resource "aws_security_group" "allow_ssh" {
 output "ec2-attributes" {
   value = "aws_instance.sample"
 }
-provider "AWS" {
+provider "aws" {
   region ="us-east-1"
 }
 
